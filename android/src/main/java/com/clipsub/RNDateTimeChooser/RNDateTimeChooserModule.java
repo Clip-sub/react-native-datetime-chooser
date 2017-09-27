13bbe4ac6ca36d481c64f3161798198ca611db34
Quan Pham <doraemonfanclub@gmail.com>
2017-09-27 16:05:11 +0700

    Minor update

--- android/src/main/java/com/clipsub/RNDateTimeChooser/RNDateTimeChooserModule.java
+++ android/src/main/java/com/clipsub/RNDateTimeChooser/RNDateTimeChooserModule.java
@@ -4,12 +4,14 @@ import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
 import com.github.florent37.singledateandtimepicker.dialog.SingleDateAndTimePickerDialog;
 
 import java.text.SimpleDateFormat;
@@ -23,7 +25,6 @@ public class RNDateTimeChooserModule extends ReactContextBaseJavaModule {
 
   public RNDateTimeChooserModule(ReactApplicationContext reactContext) {
     super(reactContext);
-    this.reactContext = reactContext;
   }
 
   @Override
@@ -36,17 +37,19 @@ public class RNDateTimeChooserModule extends ReactContextBaseJavaModule {
     final SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.FRANCE);
     String titleText = options.getString("titleText");
 
-    new SingleDateAndTimePickerDialog.Builder(this.reactContext)
+    new SingleDateAndTimePickerDialog.Builder(getCurrentActivity())
         .title(TextUtils.isEmpty(titleText) ? "Noddier" : titleText)
         .mustBeOnFuture()
         .backgroundColor(Color.WHITE)
-        .mainColor(Color.parseColor("#642580"))
+        .mainColor(Color.parseColor(titleBgColor != null ? titleBgColor : "#642580"))
         .listener(new SingleDateAndTimePickerDialog.Listener() {
           @Override
           public void onDateSelected(Date date) {
             String result = format.format(date);
-            pickedCallback.invoke(result);
-            cancelCallback.invoke(null);
+            WritableMap map = Arguments.createMap();
+            map.putString("type", "done");
+            map.putString("selectedDate", result);
+            callback.invoke(map);
           }
         }).display();
   }
